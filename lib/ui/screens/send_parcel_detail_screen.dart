3cfe80b847d6949905c1957585b5d01908afbe6a
Treenzzgod <gio12619@gmail.com>
2022-03-17 09:05:39 +0700

    detail

--- lib/ui/screens/send_parcel_detail_screen.dart
+++ lib/ui/screens/send_parcel_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parcel_shipping_courier/ui/widgets/widgets.dart';
 
 class SendParcelDetailScreen extends StatefulWidget {
   const SendParcelDetailScreen({Key? key}) : super(key: key);
@@ -24,23 +25,23 @@ class _SendParcelDetailScreenState extends State<SendParcelDetailScreen> {
             const SizedBox(
               height: 17,
             ),
-            // Text(
-            //   'Delivery method',
-            //   style: Theme.of(context).textTheme.headline3,
-            // ),
-            // const SizedBox(
-            //   height: 11,
-            // ),
-            // const MyParcelDeliveryMethod(
-            //   parcelDeliveryMethod: 'From door to parcel center',
-            //   parcelDeliveryDuration: '1 - 2 days',
-            //   parcelDeliveryImage: 'assets/images/img_door_to_parcel.png',
-            // ),
-            // const MyParcelDeliveryMethod(
-            //   parcelDeliveryMethod: 'From door to door',
-            //   parcelDeliveryDuration: '2 - 3 days',
-            //   parcelDeliveryImage: 'assets/images/img_door_to_door.png',
-            // ),
+            Text(
+              'Delivery method',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            const SizedBox(
+              height: 11,
+            ),
+            const MyParcelDeliveryMethod(
+              parcelDeliveryMethod: 'From door to parcel center',
+              parcelDeliveryDuration: '1 - 2 days',
+              parcelDeliveryImage: 'assets/images/img_door_to_parcel.png',
+            ),
+            const MyParcelDeliveryMethod(
+              parcelDeliveryMethod: 'From door to door',
+              parcelDeliveryDuration: '2 - 3 days',
+              parcelDeliveryImage: 'assets/images/img_door_to_door.png',
+            ),
           ],
         ),
       ),
